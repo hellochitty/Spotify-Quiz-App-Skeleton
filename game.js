b2c9d8feaa300fed16e9dc0214957abd65fd10bd
chithra <chithra.venkatesan.5@gmail.com>
2017-05-03 16:40:36 -0400

    Add 3 second timeout to audio

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
 $(document).ready(() => {
 
   console.log("hi");
-  $( "#click-me" ).on("click", () => {
-    console.log("fook");
+
+  $( "#play" ).on("click", () => {
+    var audio = document.getElementById("audio");
+    audio.play();
+    $( "#play" ).addClass("hidden");
+    console.log("done");
+
+  });
+  //add 3 second timeout to audio
+  var audio = document.getElementById("audio");
+  audio.onended = function() {
+    alert("audio playback has ended");
+  };
+  $(audio).on("timeupdate", (e) => {
+    if (e.timeStamp > 5000){
+      audio.pause();
+    }
   });
 });
